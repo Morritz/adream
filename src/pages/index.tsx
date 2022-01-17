a110d7da24db2701315e259b4a9a87cdebfc2041
Morritz <12800230+Morritz@users.noreply.github.com>
2022-01-17 15:16:08 +0100

    Added SCSS plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import '../styles.scss';
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <img src={'../../Glowna1/shutterstock_615439679.png'} width="100%"></img>
+    </main>
+  );
 }
